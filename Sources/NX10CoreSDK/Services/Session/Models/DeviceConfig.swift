89297665a8b63d628c89527ffd67e17dcc279484
warrdadlani-lab <warrd.adlani@nx10.me>
2026-05-06 21:01:58 +0100

    Update DeviceConfig.swift

--- Sources/NX10CoreSDK/Services/Session/Models/DeviceConfig.swift
+++ Sources/NX10CoreSDK/Services/Session/Models/DeviceConfig.swift
@@ -11,16 +11,21 @@ public struct DeviceConfig: Decodable {
     
     public let sensor: Sensor
     public let brainjuice: BrainJuiceConfig // NEW: Add brainjuice config
+    public let device: Device
 
     public struct Sensor: Decodable {
         public let touchSampleHz: Int
-        public let gyroscopeSampleHz: Int // NEW: Add gyroscopeSampleHz
-        public let accelerometerSampleHz: Int // NEW: Add accelerometerSampleHz
+        public let gyroscopeSampleHz: Int
+        public let accelerometerSampleHz: Int
+        public let keyboardTouchSampleHz: Int
+        public let acquisitionWindowSize: Int
         
-        public init(touchSampleHz: Int, gyroscopeSampleHz: Int, accelerometerSampleHz: Int) {
+        public init(touchSampleHz: Int, gyroscopeSampleHz: Int, accelerometerSampleHz: Int, keyboardTouchSampleHz: Int, acquisitionWindowSize: Int) {
             self.touchSampleHz = touchSampleHz
             self.gyroscopeSampleHz = gyroscopeSampleHz
             self.accelerometerSampleHz = accelerometerSampleHz
+            self.keyboardTouchSampleHz = keyboardTouchSampleHz
+            self.acquisitionWindowSize = acquisitionWindowSize
         }
     }
 
@@ -46,4 +51,17 @@ public struct DeviceConfig: Decodable {
             self.children = children
         }
     }
+
+    public struct Device: Decodable {
+        public let deviceModelToDpiMap: [String: Double]
+        
+        public init(deviceModelToDpiMap: [String: Double]) {
+            self.deviceModelToDpiMap = deviceModelToDpiMap
+        }
+        
+        enum CodingKeys: String, CodingKey {
+            case deviceModelToDpiMap
+        }
+    }
+
 }
