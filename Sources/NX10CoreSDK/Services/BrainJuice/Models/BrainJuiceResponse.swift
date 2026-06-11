5b467c7ccf6b58d8748e4a3d8a7ec5846a2d49a2
warrdadlani-lab <warrd.adlani@nx10.me>
2026-06-11 11:53:47 +0100

    PROD stable
    Stable

--- Sources/NX10CoreSDK/Services/BrainJuice/Models/BrainJuiceResponse.swift
+++ Sources/NX10CoreSDK/Services/BrainJuice/Models/BrainJuiceResponse.swift
@@ -17,16 +17,6 @@ extension BrainJuice {
         public let history: [HistoryEntry]
         public let realTime: HistoryEntry?
         public let metadata: MetaData?
-        public let confidenceClassification: String?
-        public let index: Double?
-        
-        enum CodingKeys: String, CodingKey {
-            case history
-            case realTime
-            case metadata
-            case confidenceClassification = "confidence_classification"
-            case index
-        }
     }
     
     public struct MetaData: Codable {
@@ -34,22 +24,24 @@ extension BrainJuice {
     }
     
     public struct HistoryEntry: Codable {
-        public let timeStart: String?
+        public let date: String?
         public let index: Int?
-        public let confidenceClassification: String?
+        public let fidelity: Double?
+        public let fidelityClassification: String?
         public let confidence: Double?
         public let confidenceTop: Int?
         public let confidenceBottom: Int?
-        
         public let subIndices: SubIndices?
-        public let subIndicesConfidence: SubIndices?
-        public let subIndicesConfidenceTop: SubIndices?
-        public let subIndicesConfidenceBottom: SubIndices?
+        public let subIndicesConfidence: SubIndicesConfidence?
+        public let subIndicesConfidenceTop: SubIndicesConfidence?
+        public let subIndicesConfidenceBottom: SubIndicesConfidence?
+        public let timestamp: String?
         
         enum CodingKeys: String, CodingKey {
-            case timeStart
+            case date
             case index
-            case confidenceClassification = "confidence_classification"
+            case fidelity
+            case fidelityClassification = "fidelity_classification"
             case confidence
             case confidenceTop = "confidence_top"
             case confidenceBottom = "confidence_bottom"
@@ -57,16 +49,33 @@ extension BrainJuice {
             case subIndicesConfidence
             case subIndicesConfidenceTop = "subIndices_confidence_top"
             case subIndicesConfidenceBottom = "subIndices_confidence_bottom"
+            case timestamp
+        }
+    }
+    
+    public struct SubIndicesConfidence: Codable {
+        public let motorStability: Double?
+        public let behaviourRestlessness: Double?
+        
+        enum CodingKeys: String, CodingKey {
+            case motorStability
+            case behaviourRestlessness
         }
     }
     
     public struct SubIndices: Codable {
-        public let cognitiveLoad: Double?
-        public let motorControl: Double?
+        public let motorStability: Double?
+        public let behaviourRestlessness: Double?
         
-        enum CodingKeys: String, CodingKey {
-            case cognitiveLoad = "cognitive_load"
-            case motorControl = "motor_control"
+        public init(motorStability: Double? = nil, behaviourRestlessness: Double? = nil) {
+            self.motorStability = motorStability
+            self.behaviourRestlessness = behaviourRestlessness
+        }
+        
+        public init(from decoder: Decoder) throws {
+            let container = try decoder.container(keyedBy: CodingKeys.self)
+            motorStability = try container.decodeIfPresent(Double.self, forKey: .motorStability)
+            behaviourRestlessness = try container.decodeIfPresent(Double.self, forKey: .behaviourRestlessness)
         }
     }
 }
